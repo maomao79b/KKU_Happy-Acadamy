fd4374bce6007c65ed75a0d51cefaae506d873cb
maomao79b <j.pbpromammothz@gmail.com>
2023-02-14 22:36:55 +0700

    change1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import Nav from './Component/Nav';
 import Login from './Component/Login';
 import './App.css';
+import { Switch, Route } from 'react-router'
 function App() {
   return (
     <div className='App'>
       <Login />
+      <Switch>
+        <Route>
+          
+        </Route>
+
+      </Switch>
     </div>
   );
 }
