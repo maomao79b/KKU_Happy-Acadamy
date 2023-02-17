308a33e294cdb7bf9fad028bd0cb6a6eb0eb92c7
watthananick1 <nick1387714169@gmail.com>
2023-02-18 00:07:53 +0700

    Update Login.js

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./../App.css";
 import "../CSS/Login.css";
-import axios from "axios";
+// import axios from "axios";
 
 function Login() {
   const [user, setUser] = useState({
